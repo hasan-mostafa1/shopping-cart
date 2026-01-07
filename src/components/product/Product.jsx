da243dead741b5b5714bbdc34ad7f36d4717938e
Hasan Mostafa <hasanalimostafa1@gmail.com>
2026-01-07 05:14:56 +0300

    Add Product component

---
+++ src/components/product/Product.jsx
@@ -0,0 +1,119 @@
+import { useState } from "react";
+import styles from "./Product.module.css";
+import cartIconUrl from "../../assets/icons/cart.svg";
+import { Link } from "react-router";
+
+export default function Product({ product, cart, setCart }) {
+  const [quantities, setQuantities] = useState({});
+
+  function increaseQunatity(productId) {
+    const quantity = quantities[productId]?.quantity || 1;
+    setQuantities({ ...quantities, [productId]: { quantity: quantity + 1 } });
+  }
+
+  function decreaseQunatity(productId) {
+    const quantity = quantities[productId]?.quantity || 1;
+    if (quantity > 0) {
+      setQuantities({
+        ...quantities,
+        [productId]: { quantity: quantity - 1 },
+      });
+    }
+  }
+
+  function handleQuantityChange(productId, e) {
+    const inputNumber = e.target.value;
+    if (
+      !inputNumber.includes("-") &&
+      !inputNumber.startsWith("0") &&
+      !inputNumber.includes(".")
+    ) {
+      setQuantities({
+        ...quantities,
+        [productId]: { quantity: e.target.value },
+      });
+    }
+  }
+
+  function addToCart(productId) {
+    setCart([
+      ...cart,
+      { productId: productId, quantity: quantities[productId]?.quantity || 1 },
+    ]);
+  }
+
+  function getFromCart(productId) {
+    let product = null;
+    cart.forEach((item) => {
+      if (item.productId === productId) {
+        product = item;
+      }
+    });
+    return product;
+  }
+  return (
+    <div className={styles.product}>
+      <Link to={`product/${product.id}`} className={styles.link}>
+        <img src={product.image} alt="" />
+      </Link>
+      <div className={styles.description}>
+        <p>{product.title}</p>
+        <span>${product.price}</span>
+      </div>
+      <form className={styles.addToCartForm}>
+        <div className={styles.quantity}>
+          <button
+            className={styles.decrease}
+            type="button"
+            onClick={() => {
+              decreaseQunatity(product.id);
+            }}
+          >
+            –
+          </button>
+          <input
+            type="number"
+            name=""
+            id=""
+            value={
+              quantities[product.id]?.quantity ||
+              getFromCart(product.id)?.quantity ||
+              "1"
+            }
+            onChange={(e) => {
+              handleQuantityChange(product.id, e);
+            }}
+          />
+          <button
+            className={styles.increase}
+            type="button"
+            onClick={() => {
+              increaseQunatity(product.id);
+            }}
+          >
+            +
+          </button>
+        </div>
+        {getFromCart(product.id) ? (
+          <button
+            type="button"
+            onClick={() => {
+              addToCart(product.id);
+            }}
+          >
+            <img src={cartIconUrl} alt="" /> Update
+          </button>
+        ) : (
+          <button
+            type="button"
+            onClick={() => {
+              addToCart(product.id);
+            }}
+          >
+            <img src={cartIconUrl} alt="" /> Add
+          </button>
+        )}
+      </form>
+    </div>
+  );
+}
